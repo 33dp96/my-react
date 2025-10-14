74e217119dfdfcebe90eb3c1ba71b6a34701b066
33dp96 <33dp96@gmail.com>
2025-10-14 08:57:17 +0330

    Create Settings-Panel

--- src/StarAdmin.js
+++ src/StarAdmin.js
@@ -1,10 +1,11 @@
 import Navbar from "./Components/StarAdmin/Navbar";
+import ThemeSettingWrapper from "./Components/StarAdmin/ThemeSettingWrapper";
 export default function StarAdmin(){
      return<>
           <div className="container-scroller">
                <Navbar/>
                <div className="container-fluid page-body-wrapper">
-                    
+                    <ThemeSettingWrapper/>                    
                </div>
           </div>
      </>
