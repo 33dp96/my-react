7f5edd50d08c2c7c2a14f5bb52d8a40d8e9be8c7
33dp96 <33dp96@gmail.com>
2025-10-14 10:24:00 +0330

    Create Sidebar

--- src/StarAdmin.js
+++ src/StarAdmin.js
@@ -1,11 +1,15 @@
 import Navbar from "./Components/StarAdmin/Navbar";
+import SettingsPanel from "./Components/StarAdmin/SettingsPanel";
+import Sidebar from "./Components/StarAdmin/Sidebar";
 import ThemeSettingWrapper from "./Components/StarAdmin/ThemeSettingWrapper";
 export default function StarAdmin(){
      return<>
           <div className="container-scroller">
                <Navbar/>
                <div className="container-fluid page-body-wrapper">
-                    <ThemeSettingWrapper/>                    
+                    <ThemeSettingWrapper/>
+                    <SettingsPanel/>
+                    <Sidebar/>                    
                </div>
           </div>
      </>
